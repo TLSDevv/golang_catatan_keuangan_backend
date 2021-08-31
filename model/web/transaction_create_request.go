c159e7919c00c3495eaa1ea2206eaccd587c3778
afandy2000 <afandywibowo2000@gmail.com>
2021-08-31 22:16:12 +0800

    feat : add web format for transaction

---
+++ model/web/transaction_create_request.go
@@ -0,0 +1,10 @@
+package web
+
+type TransactionCreateRequest struct {
+	UserId          uint8  `json:"user_id"`
+	NameTransaction string `json:"name_transaction"`
+	TypeTransaction string `json:"type_transaction"`
+	CategoryId      uint8  `json:"category_id"`
+	Amount          int64  `json:"nominal"`
+	Description     string `json:"description"`
+}
---
+++ model/web/transaction_response.go
@@ -0,0 +1,11 @@
+package web
+
+type TransactionResponse struct {
+	Id              uint8  `json:"id"`
+	UserId          uint8  `json:"user_id"`
+	NameTransaction string `json:"name_transaction"`
+	TypeTransaction string `json:"type_transaction"`
+	CategoryId      uint8  `json:"category_id"`
+	Amount          int64  `json:"nominal"`
+	Description     string `json:"description"`
+}
