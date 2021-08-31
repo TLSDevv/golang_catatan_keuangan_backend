29b0705a3aa2c4e4053a305bdbcc2ba294034d32
afandy2000 <afandywibowo2000@gmail.com>
2021-08-31 22:20:10 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/TLSDevv/golang_catatan_keuangan_backend
+
+go 1.16
+
+require (
+	github.com/go-chi/chi/v5 v5.0.3 // indirect
+	github.com/lib/pq v1.10.2 // indirect
+	github.com/spf13/viper v1.8.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigFile(`config.json`)
+	err := viper.ReadInConfig()
+
+	if err != nil {
+		panic(err)
+	}
+
+	if viper.GetBool("debug") {
+		fmt.Println("Service RUN on Debug mode")
+	}
+}
+
+func main() {
+	dbHost := viper.GetString(`database.host`)
+	dbPort := viper.GetString(`database.port`)
+	dbUser := viper.GetString(`database.user`)
+	dbPass := viper.GetString(`database.pass`)
+	dbName := viper.GetString(`database.name`)
+
+}
