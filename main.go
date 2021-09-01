ee2864cfbdfb2db2c51110ca607af4833006bc0a
afandy2000 <afandywibowo2000@gmail.com>
2021-09-01 19:00:13 +0800

    fix init

--- main.go
+++ main.go
@@ -1,8 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/TLSDevv/golang_catatan_keuangan_backend/api/mobile"
+	"github.com/TLSDevv/golang_catatan_keuangan_backend/api/mobile/controller"
+	"github.com/TLSDevv/golang_catatan_keuangan_backend/api/mobile/service"
+	"github.com/TLSDevv/golang_catatan_keuangan_backend/api/repository"
 	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 )
@@ -26,5 +33,43 @@ func main() {
 	dbUser := viper.GetString(`database.user`)
 	dbPass := viper.GetString(`database.pass`)
 	dbName := viper.GetString(`database.name`)
+	dbDriver := viper.GetString(`database.driver`)
+	serverPort := viper.GetString(`server.port`)
 
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", dbHost, dbPort, dbUser, dbPass, dbName)
+	db, err := sql.Open(dbDriver, dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		err := db.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	categoryRepository := repository.NewCategoryRepository()
+	categoryService := service.NewCategoryService(categoryRepository, db)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	userRepository := repository.NewUserRepository()
+	userService := service.NewUserService(userRepository, db)
+	userController := controller.NewUserController(userService)
+
+	transactionRepository := repository.NewTransactionRepository()
+	transactionService := service.NewTransactioService(transactionRepository, db)
+	transactionController := controller.NewTransactionController(transactionService)
+
+	handler := &mobile.Handler{}
+	handler.NewHandler(userController, categoryController, transactionController)
+
+	fmt.Printf("Listening to port %s", serverPort)
+	http.ListenAndServe(serverPort, handler.Route)
 }
