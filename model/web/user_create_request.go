212da8ea6c3a7555cef4b7693ed14e3e4e97893c
afandy2000 <afandywibowo2000@gmail.com>
2021-09-02 16:10:44 +0800

    fix: validate

--- model/web/user_create_request.go
+++ model/web/user_create_request.go
@@ -3,7 +3,7 @@ package web
 type UserCreateRequest struct {
 	Username string `validate:"required,max=100,min=1" json:"username"`
 	Name     string `validate:"required,max=100,min=1" json:"name"`
-	Gender   string `validate:"required,min=2" json:"gender"`
-	Age      int    `json:"age"`
+	Gender   string `validate:"required,min=1" json:"gender"`
+	Age      int    `validate:"required" json:"age"`
 	Job      string `validate:"required,max=100,min=1" json:"job"`
 }
