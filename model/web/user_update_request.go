2529c04134dbfc009ffa2230a420ce8c29f8e28e
afandy2000 <afandywibowo2000@gmail.com>
2021-09-02 16:21:34 +0800

    fix: validate

--- model/web/user_update_request.go
+++ model/web/user_update_request.go
@@ -4,7 +4,7 @@ type UserUpdateRequest struct {
 	Id       uint8  `validate:"required" json:"id"`
 	Username string `validate:"required,max=100,min=1" json:"username"`
 	Name     string `validate:"required,max=100,min=1" json:"name"`
-	Gender   string `validate:"required,min=2" json:"gender"`
+	Gender   string `validate:"required,min=1" json:"gender"`
 	Age      int    `validate:"required,min=1" json:"age"`
 	Job      string `validate:"required,max=100,min=1" json:"job"`
 }
