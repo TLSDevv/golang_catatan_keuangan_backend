bc924b2db8c363e709bc176377f086006a0cd50c
rasyidcode <jamil@dwebsite.net>
2022-01-28 20:06:36 +0700

    test

--- model/transaction.go
+++ model/transaction.go
@@ -1,12 +1,12 @@
 package model
 
-type Transaction struct {
-	Id      int `json:"id"`
-	TrcName int `json:"trc_name"`
-	Id      int `json:"id"`
-	Id      int `json:"id"`
-	Id      int `json:"id"`
-	Id      int `json:"id"`
-	Id      int `json:"id"`
-	Id      int `json:"id"`
-}
+// type Transaction struct {
+// 	Id      int `json:"id"`
+// 	TrcName int `json:"trc_name"`
+// 	Id      int `json:"id"`
+// 	Id      int `json:"id"`
+// 	Id      int `json:"id"`
+// 	Id      int `json:"id"`
+// 	Id      int `json:"id"`
+// 	Id      int `json:"id"`
+// }
