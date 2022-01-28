ba384b035a579dd3a2a1c81f307a9eb3d9796092
afandy2000 <afandywibowo2000@gmail.com>
2022-01-28 21:10:28 +0800

    feat(pkg): add bycrypt hash password and compare

---
+++ pkg/bycrypt.go
@@ -0,0 +1,15 @@
+package pkg
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func PasswordToHash(password string) string {
+	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(hashPassword)
+}
+
+func ComparePassword(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
