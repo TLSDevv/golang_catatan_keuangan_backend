3ff1431ec6cb01bfb9f5ff18d1b52e98112360b9
afandy2000 <afandywibowo2000@gmail.com>
2022-02-02 14:18:49 +0800

    fix(main): remove test

--- main.go
+++ main.go
@@ -1,33 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "github.com/TLSDevv/golang_catatan_keuangan_backend/cmd"
 
 func main() {
-	// cmd.Execute()
-	test := "test"
-	test += " yey"
-
-	var sb strings.Builder
-
-	sb.WriteString("test yey")
-	sb.WriteString("\ntest again")
-	fmt.Println(test)
-	fmt.Println(sb.String())
-
-	params := map[string]interface{}{}
-	params["test"] = 59
-	params["test2"] = 69
-	params["test3"] = "wei"
-	params["test4"] = "wei"
-	params["test5"] = "wei"
-
-	value, isExist := params["test6"]
-	if isExist {
-		fmt.Println(value)
-	} else {
-		fmt.Println("not exist")
-	}
+	cmd.Execute()
 }
