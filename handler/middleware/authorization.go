b071dcc0350c67234e32fce96066aafacbfb98d7
afandy2000 <afandywibowo2000@gmail.com>
2022-02-09 23:40:49 +0800

    feat(middleware): add authorization

---
+++ handler/middleware/authorization.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/TLSDevv/golang_catatan_keuangan_backend/handler/util"
+	"github.com/TLSDevv/golang_catatan_keuangan_backend/pkg"
+)
+
+func Authorization(h http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			accessDetails, err := pkg.ExtractTokenMetadata(r, pkg.AccessTokenLifeTime)
+
+			if err != nil {
+				util.SendNoData(w, http.StatusBadRequest, err.Error())
+				return
+			}
+
+			ctx := context.WithValue(context.Background(), "user_id", accessDetails.Id)
+			ctx = context.WithValue(ctx, "username", accessDetails.Username)
+			r.WithContext(ctx)
+			
+			h.ServeHTTP(w, r)
+		})
+}
