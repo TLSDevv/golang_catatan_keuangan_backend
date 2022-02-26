0ba3c2d88c9bfd5791a97c82590d0c7e24a5a8c9
rasyidcode <rasyidcode7@gmail.com>
2022-02-26 10:14:37 +0700

    add details about the change, and change 400 to 401

--- gateways/http/middleware/authorization.go
+++ gateways/http/middleware/authorization.go
@@ -14,13 +14,17 @@ func Authorization(h http.Handler) http.Handler {
 			accessDetails, err := pkg.ExtractTokenMetadata(r, pkg.TypeTokenAccess)
 
 			if err != nil {
-				util.SendNoData(w, http.StatusBadRequest, err.Error())
+				// [jamil] - change to unauthorized 401
+				util.SendNoData(w, http.StatusUnauthorized, err.Error())
 				return
 			}
 
+			// [jamil] - change context.Background() to r.Context()
 			ctx := context.WithValue(r.Context(), "user_id", accessDetails.Id)
 			ctx = context.WithValue(ctx, "username", accessDetails.Username)
 			ctx = context.WithValue(ctx, "role", accessDetails.Role)
+
+			// [jamil] - redefine the request
 			r = r.WithContext(ctx)
 
 			h.ServeHTTP(w, r)
