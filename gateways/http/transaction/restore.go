1acc260d3c6fcb04145bb9fa24ee9128dcb40224
rasyidcode <rasyidcode7@gmail.com>
2022-03-05 21:10:27 +0700

    modify restore transaction handler

--- gateways/http/transaction/restore.go
+++ gateways/http/transaction/restore.go
@@ -9,22 +9,26 @@ import (
 func (th TransactionHandler) Restore(w http.ResponseWriter, r *http.Request) {
 	tID := util.GetParams(r, "id")
 	// validate transaction_id
-	te, err := th.service.CheckTransactionByID(r.Context(), tID)
-	if err != nil {
-		_ = util.SendError(w, err.Error(), http.StatusNotFound, nil)
-		return
-	}
-	if !te {
-		_ = util.SendError(w, "transaction not found", http.StatusNotFound, nil)
-		return
-	}
+	// te, err := th.service.CheckTransactionByID(r.Context(), tID)
+	// if err != nil {
+	// 	_ = util.SendError(w, err.Error(), http.StatusNotFound, nil)
+	// 	return
+	// }
+	// if !te {
+	// 	_ = util.SendError(w, "transaction not found", http.StatusNotFound, nil)
+	// 	return
+	// }
 
 	// call restore service
-	err = th.service.Restore(r.Context(), tID)
+	err := th.service.Restore(r.Context(), tID)
 	if err != nil {
-		_ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+		// _ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+		util.SendNoData(w, http.StatusInternalServerError, err.Error())
+
 		return
 	}
 
-	_ = util.SendSuccess(w, "transaction restored successfully!", http.StatusOK, nil)
+	// _ = util.SendSuccess(w, "transaction restored successfully!", http.StatusOK, nil)
+	util.SendNoData(w, http.StatusOK, "Transaction restored successfully!")
+	return
 }
