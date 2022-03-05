5bf7c2d245bf28af7d6f4748664509cf2328f5fd
rasyidcode <rasyidcode7@gmail.com>
2022-03-05 21:03:21 +0700

    remove comments from transaction list handler

--- gateways/http/transaction/list.go
+++ gateways/http/transaction/list.go
@@ -9,14 +9,11 @@ import (
 func (th TransactionHandler) List(w http.ResponseWriter, r *http.Request) {
 	transactions, err := th.service.GetTransactions(r.Context())
 	if err != nil {
-		// _ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
-		// return
 		util.SendNoData(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	transactionsResponse := formatSliceResponse(transactions)
-	// _ = util.Send(w, TransactionListResponse{Success: true, Data: transactionsResponse}, http.StatusOK)
 	util.SendWithData(w, http.StatusOK, "", transactionsResponse)
 	return
 }
