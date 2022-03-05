1d482da7e374f415d7b4d24ece9e6467c06b3e51
rasyidcode <rasyidcode7@gmail.com>
2022-03-05 21:06:02 +0700

    remove comments from get by id transaction handler

--- gateways/http/transaction/get_by_id.go
+++ gateways/http/transaction/get_by_id.go
@@ -10,19 +10,10 @@ func (th TransactionHandler) GetByID(w http.ResponseWriter, r *http.Request) {
 	tID := util.GetParams(r, "id")
 	t, err := th.service.GetByID(r.Context(), tID)
 	if err != nil {
-		// _ = util.SendError(w, "No transaction found", http.StatusNotFound, nil)
 		util.SendNoData(w, http.StatusNotFound, "No transaction found")
 		return
 	}
 
-	// _ = util.SendSuccess(w, "", http.StatusOK, ResponseBody{
-	// 	ID:              t.ID,
-	// 	TransactionName: t.TransactionName,
-	// 	Category:        t.Category,
-	// 	TransactionType: t.TransactionType,
-	// 	TransactionAt:   t.TransactionAt,
-	// 	CreatedAt:       t.CreatedAt,
-	// })
 	util.SendWithData(w, http.StatusOK, "", ResponseBody{
 		ID:              t.ID,
 		TransactionName: t.TransactionName,
