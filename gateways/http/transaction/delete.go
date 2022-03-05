8d04a8e2c8370dff7346ab40fbf8fa1c2ee6f086
rasyidcode <rasyidcode7@gmail.com>
2022-03-05 21:07:18 +0700

    modify delete transaction handler

--- gateways/http/transaction/delete.go
+++ gateways/http/transaction/delete.go
@@ -12,9 +12,12 @@ func (th TransactionHandler) Delete(w http.ResponseWriter, r *http.Request) {
 	// call delete service
 	err := th.service.Delete(r.Context(), tID)
 	if err != nil {
-		_ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+		// _ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+		util.SendNoData(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	_ = util.SendSuccess(w, "transaction deleted successfully!", http.StatusOK, nil)
+	// _ = util.SendSuccess(w, "transaction deleted successfully!", http.StatusOK, nil)
+	util.SendNoData(w, http.StatusOK, "Transaction deleted successfully!")
+	return
 }
