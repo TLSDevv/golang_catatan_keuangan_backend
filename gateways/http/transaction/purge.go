2ffa1a3cc33a7e7cc70f2936e0bb3741b5543fec
rasyidcode <rasyidcode7@gmail.com>
2022-03-05 21:13:05 +0700

    modify purge transaction handler

--- gateways/http/transaction/purge.go
+++ gateways/http/transaction/purge.go
@@ -9,22 +9,25 @@ import (
 func (th TransactionHandler) Purge(w http.ResponseWriter, r *http.Request) {
 	tID := util.GetParams(r, "id")
 	// validate transaction_id
-	te, err := th.service.CheckTransactionByID(r.Context(), tID)
-	if err != nil {
-		_ = util.SendError(w, err.Error(), http.StatusNotFound, nil)
-		return
-	}
-	if !te {
-		_ = util.SendError(w, "transaction not found", http.StatusNotFound, nil)
-		return
-	}
+	// te, err := th.service.CheckTransactionByID(r.Context(), tID)
+	// if err != nil {
+	// 	_ = util.SendError(w, err.Error(), http.StatusNotFound, nil)
+	// 	return
+	// }
+	// if !te {
+	// 	_ = util.SendError(w, "transaction not found", http.StatusNotFound, nil)
+	// 	return
+	// }
 
 	// call purge service
-	err = th.service.Purge(r.Context(), tID)
+	err := th.service.Purge(r.Context(), tID)
 	if err != nil {
-		_ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+		// _ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+		util.SendNoData(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	_ = util.SendSuccess(w, "transaction purged successfully!", http.StatusOK, nil)
+	// _ = util.SendSuccess(w, "transaction purged successfully!", http.StatusOK, nil)
+	util.SendNoData(w, http.StatusOK, "Transaction purged successfully!")
+	return
 }
