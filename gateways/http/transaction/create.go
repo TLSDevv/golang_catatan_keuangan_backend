9a40fd3c378db1b698692b7f4a2edd2e3f95cedf
rasyidcode <rasyidcode7@gmail.com>
2022-03-05 10:07:02 +0700

    remove comments on create transaction handler

--- gateways/http/transaction/create.go
+++ gateways/http/transaction/create.go
@@ -12,7 +12,6 @@ func (th TransactionHandler) Create(w http.ResponseWriter, r *http.Request) {
 
 	err := util.Decode(r, &reqBody)
 	if err != nil {
-		// _ = util.SendError(w, err.Error(), http.StatusBadRequest, nil)
 		util.SendNoData(w, http.StatusBadRequest, err.Error())
 		return
 	}
@@ -20,24 +19,9 @@ func (th TransactionHandler) Create(w http.ResponseWriter, r *http.Request) {
 	var reqValidation TransactionRequestValidationError
 	err = th.validator.Validate(reqBody, reqValidation)
 	if err != nil {
-		// _ = util.SendError(w, util.ErrValidation, http.StatusUnprocessableEntity, reqValidation)
 		util.SendWithData(w, http.StatusUnprocessableEntity, util.ErrValidation, reqValidation)
 		return
 	}
-
-	// #1 - check user_id exist or not
-	// #2 - dont have to check userID, the reason why is because if user can access this transaction it means
-	// the user exist
-	// =================
-	// userExist, err := th.service.CheckUser(r.Context(), r.Context().Value("user_id").(int))
-	// if err != nil {
-	// 	_ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
-	// 	return
-	// }
-	// if !userExist {
-	// 	_ = util.SendError(w, util.ErrUserNotFound, http.StatusNotFound, nil)
-	// 	return
-	// }
 
 	err = th.service.Create(r.Context(), entities.TransactionInput{
 		TransactionName: reqBody.TransactionName,
@@ -47,11 +31,9 @@ func (th TransactionHandler) Create(w http.ResponseWriter, r *http.Request) {
 		TransactionAt:   reqBody.TransactionAt,
 	})
 	if err != nil {
-		// _ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
 		util.SendNoData(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	// _ = util.SendSuccess(w, "Transaction created successfully!", http.StatusOK, nil)
 	util.SendNoData(w, http.StatusOK, "Transaction created successfully!")
 }
