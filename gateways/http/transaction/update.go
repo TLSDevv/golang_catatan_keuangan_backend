2b36e5ba53291cdefa4517bd519d6c21cde6baf3
rasyidcode <rasyidcode7@gmail.com>
2022-03-05 20:55:51 +0700

    remove comments update transaction handler

--- gateways/http/transaction/update.go
+++ gateways/http/transaction/update.go
@@ -17,41 +17,12 @@ func (th TransactionHandler) Update(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// errs := reqBody.Validate()
-	// if errs != nil {
-	// 	_ = util.SendError(w, util.ErrValidation, http.StatusUnprocessableEntity, errs)
-	// 	return
-	// }
-
 	var reqValidation TransactionRequestValidationError
 	err = th.validator.Validate(reqBody, reqValidation)
 	if err != nil {
 		util.SendWithData(w, http.StatusUnprocessableEntity, util.ErrValidation, reqValidation)
 		return
 	}
-
-	/* validate user_id, no need to check user_id, we assume that the user_id is already exist */
-	// ue, err := th.service.CheckUser(r.Context(), r.Context().Value("user_id").(int))
-	// if err != nil {
-	// 	_ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
-	// 	return
-	// }
-	// if !ue {
-	// 	_ = util.SendError(w, util.ErrUserNotFound, http.StatusNotFound, nil)
-	// 	return
-	// }
-
-	/* validate transaction_id, no need to check the transaction_id, we assume
-	that the transaction_id already exist on the db */
-	// te, err := th.service.CheckTransactionByID(r.Context(), tID)
-	// if err != nil {
-	// 	_ = util.SendError(w, err.Error(), http.StatusNotFound, nil)
-	// 	return
-	// }
-	// if !te {
-	// 	_ = util.SendError(w, "transaction not found", http.StatusNotFound, nil)
-	// 	return
-	// }
 
 	// call update service
 	err = th.service.Update(r.Context(), entities.TransactionInput{
