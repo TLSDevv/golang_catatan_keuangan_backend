5146a880b7212b00abde6dc397b23eb42c51e7a4
rasyidcode <rasyidcode7@gmail.com>
2022-03-05 21:03:02 +0700

    modify transaction list handler

--- gateways/http/transaction/list.go
+++ gateways/http/transaction/list.go
@@ -1,20 +1,22 @@
 package transaction
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/TLSDevv/golang_catatan_keuangan_backend/gateways/http/util"
 )
 
 func (th TransactionHandler) List(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Context().Value("user_id"))
 	transactions, err := th.service.GetTransactions(r.Context())
 	if err != nil {
-		_ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+		// _ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+		// return
+		util.SendNoData(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	transactionsResponse := formatSliceResponse(transactions)
-	_ = util.Send(w, TransactionListResponse{Success: true, Data: transactionsResponse}, http.StatusOK)
+	// _ = util.Send(w, TransactionListResponse{Success: true, Data: transactionsResponse}, http.StatusOK)
+	util.SendWithData(w, http.StatusOK, "", transactionsResponse)
+	return
 }
