3bb4cf884693c1acf993162a394cf7009ad1635d
rasyidcode <rasyidcode7@gmail.com>
2022-03-05 21:05:39 +0700

    modify get by id transaction handler

--- gateways/http/transaction/get_by_id.go
+++ gateways/http/transaction/get_by_id.go
@@ -10,11 +10,20 @@ func (th TransactionHandler) GetByID(w http.ResponseWriter, r *http.Request) {
 	tID := util.GetParams(r, "id")
 	t, err := th.service.GetByID(r.Context(), tID)
 	if err != nil {
-		_ = util.SendError(w, "No transaction found", http.StatusNotFound, nil)
+		// _ = util.SendError(w, "No transaction found", http.StatusNotFound, nil)
+		util.SendNoData(w, http.StatusNotFound, "No transaction found")
 		return
 	}
 
-	_ = util.SendSuccess(w, "", http.StatusOK, ResponseBody{
+	// _ = util.SendSuccess(w, "", http.StatusOK, ResponseBody{
+	// 	ID:              t.ID,
+	// 	TransactionName: t.TransactionName,
+	// 	Category:        t.Category,
+	// 	TransactionType: t.TransactionType,
+	// 	TransactionAt:   t.TransactionAt,
+	// 	CreatedAt:       t.CreatedAt,
+	// })
+	util.SendWithData(w, http.StatusOK, "", ResponseBody{
 		ID:              t.ID,
 		TransactionName: t.TransactionName,
 		Category:        t.Category,
@@ -22,4 +31,5 @@ func (th TransactionHandler) GetByID(w http.ResponseWriter, r *http.Request) {
 		TransactionAt:   t.TransactionAt,
 		CreatedAt:       t.CreatedAt,
 	})
+	return
 }
