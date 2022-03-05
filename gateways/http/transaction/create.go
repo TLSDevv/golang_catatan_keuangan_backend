4879f224c489bf6962a8aa49da1ad91cac5de528
rasyidcode <rasyidcode7@gmail.com>
2022-03-05 10:06:19 +0700

    modify create transaction handler

--- gateways/http/transaction/create.go
+++ gateways/http/transaction/create.go
@@ -8,36 +8,50 @@ import (
 )
 
 func (th TransactionHandler) Create(w http.ResponseWriter, r *http.Request) {
-	var reqBody entities.TransactionInput
+	var reqBody TransactionRequest
 
 	err := util.Decode(r, &reqBody)
 	if err != nil {
-		_ = util.SendError(w, err.Error(), http.StatusBadRequest, nil)
+		// _ = util.SendError(w, err.Error(), http.StatusBadRequest, nil)
+		util.SendNoData(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	errs := reqBody.Validate()
-	if errs != nil {
-		_ = util.SendError(w, util.ErrValidation, http.StatusUnprocessableEntity, errs)
-		return
-	}
-
-	// check user_id exist or not
-	userExist, err := th.service.CheckUser(r.Context(), r.Context().Value("user_id").(int))
+	var reqValidation TransactionRequestValidationError
+	err = th.validator.Validate(reqBody, reqValidation)
 	if err != nil {
-		_ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
-		return
-	}
-	if !userExist {
-		_ = util.SendError(w, util.ErrUserNotFound, http.StatusNotFound, nil)
+		// _ = util.SendError(w, util.ErrValidation, http.StatusUnprocessableEntity, reqValidation)
+		util.SendWithData(w, http.StatusUnprocessableEntity, util.ErrValidation, reqValidation)
 		return
 	}
 
-	err = th.service.Create(r.Context(), reqBody)
+	// #1 - check user_id exist or not
+	// #2 - dont have to check userID, the reason why is because if user can access this transaction it means
+	// the user exist
+	// =================
+	// userExist, err := th.service.CheckUser(r.Context(), r.Context().Value("user_id").(int))
+	// if err != nil {
+	// 	_ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+	// 	return
+	// }
+	// if !userExist {
+	// 	_ = util.SendError(w, util.ErrUserNotFound, http.StatusNotFound, nil)
+	// 	return
+	// }
+
+	err = th.service.Create(r.Context(), entities.TransactionInput{
+		TransactionName: reqBody.TransactionName,
+		Category:        reqBody.Category,
+		TransactionType: reqBody.TransactionType,
+		Amount:          reqBody.Amount,
+		TransactionAt:   reqBody.TransactionAt,
+	})
 	if err != nil {
-		_ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+		// _ = util.SendError(w, err.Error(), http.StatusInternalServerError, nil)
+		util.SendNoData(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	_ = util.SendSuccess(w, "Transaction created successfully!", http.StatusOK, nil)
+	// _ = util.SendSuccess(w, "Transaction created successfully!", http.StatusOK, nil)
+	util.SendNoData(w, http.StatusOK, "Transaction created successfully!")
 }
